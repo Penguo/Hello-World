562b640f19a9a3590a199ac0f5439bff4260375c
Muz Husain <muz@chartboost.com>
2017-09-04 02:09:44 -0700

    Global hotkey support
    - Updated manifest with global hotkey support
    - Listener for new hotkey
    - Replaced double w/ single quotes

--- background.js
+++ background.js
@@ -1,23 +1,39 @@
 
 chrome.browserAction.setTitle({
-    title: "For those good guy moments"
+    title: 'For those good guy moments'
 });
 
 // Load song
 var curbYourTheme = new Audio();
-curbYourTheme.src = "audio/frolic.mp3";
+curbYourTheme.src = 'audio/frolic.mp3';
 
 var currentlyPlaying = false;
-
 chrome.browserAction.onClicked.addListener(function(tab) {
     if (currentlyPlaying == false) {
-            curbYourTheme.src = "audio/frolic.mp3"; // Reload audio each time
+            curbYourTheme.src = 'audio/frolic.mp3'; // Reload audio each time
             curbYourTheme.play();
-            console.log("Enjoy!");
+            console.log('Enjoy!');
             currentlyPlaying = true;
     } else {
         curbYourTheme.pause();
-        console.log("Paused!");
+        console.log('Paused!');
         currentlyPlaying = false;
+    }
+});
+
+// Listener for global hotkey
+var globalPlayKey = 'toggleMuzak';
+chrome.commands.onCommand.addListener(function(command) {
+    if (command == globalPlayKey) {
+        if (currentlyPlaying == false) {
+            curbYourTheme.src = 'audio/frolic.mp3';
+            curbYourTheme.play();
+            console.log('Enjoy!');
+            currentlyPlaying = true;
+        } else {
+            curbYourTheme.pause();
+            console.log('Paused!');
+            currentlyPlaying = false;
+        }
     }
 });
